46914f45bc7776d4925f3b778fc005f726133c50
Anish <hney1000.ay1@gmail.com>
2020-07-14 20:48:55 +0530

    added 2 works

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ window.onload = changeBG
 const navbar = document.getElementById('navbar')
 const home = document.getElementById('home')
 const about = document.getElementById('about')
+const work1 = document.getElementById('work-1')
+const work2 = document.getElementById('work-2')
 
 const showNavbar = (color) => {
     navbar.classList.remove('hide')
@@ -21,15 +23,27 @@ function changeBG() {
     //console.log(scrollTop)
     var homefromTop = home.offsetTop - scrollTop
     var aboutfromTop = about.offsetTop - scrollTop
-    console.log(Math.abs(homefromTop) < home.clientHeight/2)
+    var work1fromTop = work1.offsetTop - scrollTop
+    var work2FromTop = work2.offsetTop - scrollTop
+    console.log(Math.abs(aboutfromTop) < about.clientHeight/2)
     if(Math.abs(homefromTop) < home.clientHeight/2){
         document.body.classList.remove('bg-dark','bg-2','bg-1')
         hideNavbar()
     }
-    else if (aboutfromTop < clinetHeight/2) {
+    else if (Math.abs(aboutfromTop) < about.clientHeight/2) {
         document.body.classList.remove('bg-dark','bg-2','bg-1')
         document.body.classList.add('bg-dark')
         showNavbar('bg-dark')
+    }
+    else if (Math.abs(work1fromTop) < work1.clientHeight/2) {
+        document.body.classList.remove('bg-dark','bg-2','bg-1')
+        document.body.classList.add('bg-1')
+        showNavbar('bg-1')
+    }
+    else if (Math.abs(work2FromTop) < work2.clientHeight/2) {
+        document.body.classList.remove('bg-dark','bg-2','bg-1')
+        document.body.classList.add('bg-2')
+        showNavbar('bg-2')
     }
     else {
         document.body.classList.remove('bg-1','bg-dark','bg-2')
