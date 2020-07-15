6611fc6b88be12804764c3d158e1dd473630069b
Anish <hney1000.ay1@gmail.com>
2020-07-15 12:35:16 +0530

    added the footer

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ const about = document.getElementById('about')
 const work1 = document.getElementById('work-1')
 const work2 = document.getElementById('work-2')
 const work3 = document.getElementById('work-3')
+const work4 = document.getElementById('work-4')
+const footer = document.getElementById('footer')
 const navItems = document.getElementsByClassName('nav-link')
 const skills = document.querySelectorAll('.chart__bar')
 
@@ -24,7 +26,7 @@ const navItemWhite = () => {
 }
 const showNavbar = (color) => {
     navbar.classList.remove('hide')
-    navbar.classList.remove('bg-dark', 'bg-1', 'bg-2', 'bg-3')
+    navbar.classList.remove('bg-dark', 'bg-1', 'bg-2', 'bg-3','bg-4','bg-5')
     navbar.classList.add(color)
 }
 const hideNavbar = () => {
@@ -52,40 +54,53 @@ function changeBG() {
     var work1fromTop = work1.offsetTop - scrollTop
     var work2FromTop = work2.offsetTop - scrollTop
     var work3FromTop = work3.offsetTop - scrollTop
+    var work4FromTop = work4.offsetTop - scrollTop
+    var footerFromTop = footer.offsetTop - scrollTop
     // console.log(Math.abs(aboutfromTop) < about.clientHeight/2)
     if (Math.abs(homefromTop) < home.clientHeight / 2) {
-        document.body.classList.remove('bg-dark', 'bg-2', 'bg-1')
+        document.body.classList.remove('bg-dark', 'bg-2', 'bg-1','bg-4','bg-3','bg-5')
         hideNavbar()
     }
     else if (Math.abs(aboutfromTop) < about.clientHeight / 2) {
-        document.body.classList.remove('bg-dark', 'bg-2', 'bg-1', 'bg-3')
+        document.body.classList.remove('bg-dark', 'bg-2', 'bg-1', 'bg-3','bg-4','bg-5')
         document.body.classList.add('bg-dark')
         showNavbar('bg-dark')
         navItemWhite()
     }
     else if (Math.abs(work1fromTop) < work1.clientHeight / 2) {
-        document.body.classList.remove('bg-dark', 'bg-2', 'bg-1', 'bg-3')
+        document.body.classList.remove('bg-dark', 'bg-2', 'bg-1', 'bg-3','bg-4','bg-5')
         document.body.classList.add('bg-1')
         showNavbar('bg-1')
         navItemWhite()
     }
     else if (Math.abs(work2FromTop) < work2.clientHeight / 2) {
-        document.body.classList.remove('bg-dark', 'bg-2', 'bg-1', 'bg-3')
+        document.body.classList.remove('bg-dark', 'bg-2', 'bg-1', 'bg-3','bg-4','bg-5')
         document.body.classList.add('bg-2')
         showNavbar('bg-2')
         navItemDark()
     }
     else if (Math.abs(work3FromTop) < work3.clientHeight / 2) {
-        document.body.classList.remove('bg-dark', 'bg-2', 'bg-1', 'bg-3')
+        document.body.classList.remove('bg-dark', 'bg-2', 'bg-1', 'bg-3','bg-4','bg-5')
         document.body.classList.add('bg-3')
         showNavbar('bg-3')
         navItemDark()
     }
+    else if (Math.abs(work4FromTop) < work4.clientHeight / 2) {
+        document.body.classList.remove('bg-dark', 'bg-2', 'bg-1', 'bg-3','bg-4','bg-5')
+        document.body.classList.add('bg-4')
+        showNavbar('bg-4')
+        navItemDark()
+    }
+    else if (Math.abs(footerFromTop) < footer.clientHeight / 2) {
+        document.body.classList.remove('bg-dark', 'bg-2', 'bg-1', 'bg-3','bg-4','bg-5')
+        document.body.classList.add('bg-5')
+        showNavbar('bg-5')
+        navItemDark()
+    }
     else {
-        document.body.classList.remove('bg-1', 'bg-dark', 'bg-2', 'bg-3')
-        document.body.classList.add('bg-1')
-        showNavbar('bg-1')
-
+        document.body.classList.remove('bg-1', 'bg-dark', 'bg-2', 'bg-3','bg-4','bg-5')
+        document.body.classList.add('bg-dark')
+        showNavbar('bg-dark')
     }
 }
 
@@ -93,7 +108,7 @@ function changeBG() {
 
 const scrollToWork = () => {
     window.scrollTo({
-        top: work1.offsetTop - document.documentElement.scrollTop,
+        top: work2.offsetTop - document.documentElement.scrollTop,
         behavior: 'smooth'
     })
 }
@@ -104,7 +119,7 @@ $(document).ready(() => {
     var wh = $(window).height();
     console.log(wh)
     $('.chart__bar').waypoint( function (direction) {
-        console.log(this)
+        //console.log(this)
             $(this).addClass('chart__bar__final')
         },{
            offset:500
